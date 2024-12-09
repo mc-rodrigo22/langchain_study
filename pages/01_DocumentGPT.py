8cac28f07e3b8f78510f0abbcabe3960debada0b
mc-rodrigo22 <133115982+mc-rodrigo22@users.noreply.github.com>
2024-12-09 14:43:14 +0000

    7.8 done

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,15 +1,22 @@
-import time
+from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import UnstructuredFileLoader
 from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.storage import LocalFileStore
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
 import streamlit as st
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📃",
 )
+
+llm = ChatOpenAI(
+    temperature=0.1,
+)
+
 
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
@@ -31,15 +38,41 @@ def embed_file(file):
     retriever = vectorstore.as_retriever()
     return retriever
 
+
 def send_message(message, role, save=True):
     with st.chat_message(role):
         st.markdown(message)
     if save:
-        st.session_state["messages"].append({"message":message, "role":role})
+        st.session_state["messages"].append({"message": message, "role": role})
+
 
 def paint_history():
     for message in st.session_state["messages"]:
-        send_message(message["message"], message["role"], save=False)
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            """,
+        ),
+        ("human", "{question}"),
+    ]
+)
+
 
 st.title("DocumentGPT")
 
@@ -49,7 +82,7 @@ Welcome!
             
 Use this chatbot to ask questions to an AI about your files!
 
-Upload your files on the sidebar!
+Upload your files on the sidebar.
 """
 )
 
@@ -64,10 +97,18 @@ if file:
     send_message("I'm ready! Ask away!", "ai", save=False)
     paint_history()
     message = st.chat_input("Ask anything about your file...")
-
     if message:
         send_message(message, "human")
-        send_message("lalala","ai")
+        chain = (
+            {
+                "context": retriever | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
 
-    else:
-        st.session_state["messages"] = []
+else:
+    st.session_state["messages"] = []
