d5de3ff81761b28f06daa04174e6974fb467d4ff
mc-rodrigo22 <133115982+mc-rodrigo22@users.noreply.github.com>
2024-12-23 13:33:58 +0000

    8.0 setting

--- pages/03_PrivateGPT.py
+++ pages/03_PrivateGPT.py
@@ -41,10 +41,10 @@ llm = ChatOpenAI(
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
+    file_path = f"./.cache/private_files/{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f"./.cache/private_embeddings/{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
